c3c92011a5f54c10a5b7ef613ef437d88fecd95a
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-02-07 13:24:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
-require(["esri/Map", "esri/views/SceneView"], function (Map, SceneView) {
-        var map = new Map({
-          basemap: "topo-vector",
-          ground: "world-elevation"
-        });
+require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/widgets/Legend"], (
+      Map,
+      CSVLayer,
+      MapView,
+      Legend
+    ) => {
+  const url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          scale: 50000000,
-          center: [15, 65] // longitude, latitude
-        });
+      // Paste the url into a browser's address bar to download and view the attributes
+
+
+  const template = {
+   title: "Crime committed at {ILEADSStreet}."
+};
+  
+  const renderer = {
+          type: "heatmap",
+  colorStops: [
+    { ratio: 0, color: "rgba(255, 255, 255, 0)" },
+    { ratio: 0.1, color: "rgba(255, 225, 0, 1)" },
+    { ratio: 0.2, color: "rgba(255, 200, 0, 1)" },
+    { ratio: 0.3, color: "rgba(255, 175, 0, 1)" },
+    { ratio: 0.4, color: "rgba(255, 150, 0, 1)" },
+    { ratio: 0.5, color: "rgba(255, 125, 0, 1)" },
+    { ratio: 0.6, color: "rgba(255, 100, 0, 1)" },
+    { ratio: 0.7, color: "rgba(255, 75, 0, 1)" },
+    { ratio: 0.8, color: "rgba(255, 50, 0, 1)" },
+    { ratio: 0.9, color: "rgba(255, 25, 0, 1)" },
+    { ratio: 1, color: "rgba(255, 0, 0, 1)" }
+  ],
+  minPixelIntensity: 0,
+  maxPixelIntensity: 5000
+};
+
+     const layer = new CSVLayer({
+        url: url,
+        title: "St. Louis Crime Heatmap",
+        copyright: "St. Louis Police Department",
+		latitudeField:"Latitude",
+        longitudeField:"Longitude",
+		popupTemplate: template,
+		renderer: renderer
+});
+
+      const map = new Map({
+        basemap: "gray",
+        layers: [layer]
       });
+
+      const view = new MapView({
+        container: "viewDiv",
+        center: [-90.18, 38.672],
+        zoom: 11,
+        map: map
+      });
+        view.ui.add(
+          new Legend({
+            view: view
+          }),
+          "bottom-left"
+        );
+    });
